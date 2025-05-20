90f1c476f672b923132ebb7570ba6ceed7c7d8ed
Cem <66869029+cemGr@users.noreply.github.com>
2025-05-20 17:58:55 +0000

    second push is just for fun

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import folium
 import os
 
-# CSV-Datei einlesen
+
 CSV_PATH = os.path.join("data", "metro-bike-share-stations-2025-04-01.csv")
 
 # CSV laden (latin1 wegen Sonderzeichen in Spalte 'status2')
@@ -11,7 +11,7 @@ df = pd.read_csv(CSV_PATH, encoding='latin1')
 # Ungültige Koordinaten (z. B. 0.0) filtern
 df = df[(df['Latitude'] != 0.0) & (df['Longitude'] != 0.0)]
 
-# Karte auf Mittelwert zentrieren
+# Karte auf Mittelwert zentrieren. Grund: user freundlichkeit
 center_lat = df['Latitude'].mean()
 center_lon = df['Longitude'].mean()
 m = folium.Map(location=[center_lat, center_lon], zoom_start=14)
